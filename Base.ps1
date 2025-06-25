0595c30ea55493d4a2d2eb44516c040e4a0ee714
ShadoowCK <111450796+ShadoowCK@users.noreply.github.com>
2025-06-25 10:30:12 -0300

    Delete Base.ps1

--- Base.ps1
+++
@@ -1,8 +0,0 @@
-# winutil.ps1
-Write-Host "Instalando Google Chrome..."
-Start-Process "https://dl.google.com/chrome/install/latest/chrome_installer.exe" -Wait
-
-Write-Host "Instalando 7-Zip..."
-Start-Process "https://www.7-zip.org/a/7z2301-x64.exe" -Wait
-
-Write-Host "Tudo instalado com sucesso!"
